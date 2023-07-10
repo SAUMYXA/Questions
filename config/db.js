e4c41fe21c619b36b790545d6dc8a67aac449179
SaumyaSrivastava <saumya2113061@akgec.ac.in>
2023-07-11 02:00:41 +0530

    Connected to MongoDb database

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose=require("mongoose");
+const connectDB= async()=>{
+    try{
+      const connect= await mongoose.connect(process.env.CONNECTION_STRING);
+      console.log("Successful connnection of MongoDB database at",connect.connection.host,connect.connection.name);
+    }
+    catch(error){
+        console.log(error);
+        process.exit(1);
+    }
+}
+module.exports=connectDB;
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express= require('express');
+const dotenv=require("dotenv").config();
+const connectDB=require("./config/db");
 const app=express();
+connectDB();
 const port=process.env.PORT || 6000;
 app.listen(port,()=>{
 console.log(`Server running at port ${port}`)
